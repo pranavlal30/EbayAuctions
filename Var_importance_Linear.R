07f6e516873e706b3af752f4c5eaea52546fbf0b
raphyx <rcohen6@hawk.iit.edu>
2018-11-30 20:15:37 -0600

    updates

--- Var_importance_Linear.R
+++ Var_importance_Linear.R
@@ -29,10 +29,10 @@ control <- trainControl(method="cv", number=3)
 # model <- train(Price~., data=EbayAuctions, method='bridge', 
 #                preProcess="scale", trControl=control, metric = 'RMSE')
 
-# model <- train(Price~., data=EbayAuctions[1:10000], method='bridge', 
-#                trControl=control, metric = 'RMSE')
-model <- train(Price~., data=EbayAuctions, method='foba', 
+model <- train(Price~., data=EbayAuctions[1:10000], method='bridge',
                trControl=control, metric = 'RMSE')
+# model <- train(Price~., data=EbayAuctions, method='foba', 
+#                trControl=control, metric = 'RMSE')
 
 
 # estimate variable importance
@@ -44,6 +44,35 @@ plot(importance, cex= 1.2, cex.main = 1.5,
      main = 'Var importance with random forest',cex.lab=1.5, cex.axis=2,
      cex.names=2)
 
+
+
+
+
+
+prin_comp <- prcomp(EbayAuctions[,-c('Price')])
+biplot(prin_comp, scale = 0)
+
+pr_var <- prin_comp$sdev^2
+prop_varex <- pr_var/sum(pr_var)
+
+plot(cumsum(prop_varex), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     type = "b")
+
+comp <- as.data.table(prin_comp$x)
+# df.pca <- cbind(EbayAuctions[,'Price'], comp[,c('PC1', 'PC2', 'PC3')])
+df.pca <- cbind(EbayAuctions[,'Price'], comp)
+reg.pca <- lm(formula = Price ~ PC1 + PC2 + PC3 + I(PC1*PC2) + 
+                I(PC1**2) + I(PC2**2) + I(PC3**2) + I(PC1*PC3) + I(PC2*PC3), 
+              data = df.pca)
+summary(reg.pca)
+
+dd <- ggplot(data = df.pca, aes(x = PC1, y = PC2, color = Price)) + geom_point()
+dd
+
+
+
+ggplot(data = EbayAuctions, aes(x = StartingBid, y = AuctionMedianPrice, color = Price)) + geom_point()
 
 # 
 # test.data[["Interval"]] <- (round((test.data$Prediction)/5+1)*5)
